396f6e2b6b089c8c8bdb508e78c0757df1a9a1ca
François Kooman <fkooman@tuxed.net>
2012-03-09 10:42:12 +0100

    use sun java for now as some openjdk bugs are triggered by shindig or deps

--- conext_install.sh
+++ conext_install.sh
@@ -35,8 +35,17 @@ yum -y update
 yum -y install tomcat6 httpd php-cli php php-mysql php-curl svn telnet \
 	mod_ssl mysql-server openldap-servers php-ldap php-xml wget \
 	firefox mysql-connector-java memcached openldap-clients \
-	php-pecl-memcache nss-tools java-1.6.0-openjdk-devel gcc \
-	kernel-devel git
+	php-pecl-memcache nss-tools gcc kernel-devel git \
+	java-1.6.0-openjdk-devel
+
+# We need Sun's Java for now unfortunately due to OpenJDK bugs
+# See http://bugs.openjdk.java.net/show_bug.cgi?id=100167
+wget -N http://download.oracle.com/otn-pub/java/jdk/6u31-b04/jdk-6u31-linux-x64-rpm.bin
+sh jdk-6u31-linux-x64-rpm.bin -noregister
+JAVA_HOME=/usr/java/latest
+export JAVA_HOME
+PATH=/usr/java/latest/bin:$PATH
+export PATH
 
 # install non-CentOS packages
 yum -y install shibboleth
