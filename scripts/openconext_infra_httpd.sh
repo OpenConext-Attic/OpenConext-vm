c62d4703c3b39a6aab53dcd88fd8980096dd433e
Boy Baukema <boy@ibuildings.nl>
2012-06-12 14:48:48 +0200

    Simplify and improve configuration management

--- scripts/openconext_infra_httpd.sh
+++ scripts/openconext_infra_httpd.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
-cd /tmp &&
-svn export -q https://svn.surfnet.nl/svn/coin-os/coin-infra/trunk/coin-infra-dist/src/main/ infra
-
 # Apache Configuration
 rm -f /etc/httpd/conf.d/* &&
-cp -f infra/httpd/conf.d/php/demo/*  /etc/httpd/conf.d/ &&
-cp -f infra/httpd/conf.d/java/demo/* /etc/httpd/conf.d/ &&
-rm -rf infra &&
-
-cd - &&
+cp -f /vagrant/configs/httpd/conf.d/*  /etc/httpd/conf.d/ &&
 
 ###########################
 # Install SSL certificate #
 ###########################
 
-# The certificates were generated using the XCA tool, the DB file is included
-# as well as "openconext.xdb".
-
 rm -rf /etc/httpd/keys &&
 mkdir -p /etc/httpd/keys/ &&
-cp /vagrant/certs/* /etc/httpd/keys/ &&
+cp /vagrant/certs/*.* /etc/httpd/keys/ &&
 
 service httpd restart
