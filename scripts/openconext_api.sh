863c5694af550b1ebb753d47ba3349b4d9abe705
Alle Veenstra <alle.veenstra@gmail.com>
2012-06-25 16:54:29 +0200

    Teams in janus

--- scripts/openconext_api.sh
+++ scripts/openconext_api.sh
@@ -2,6 +2,9 @@
 
 if [ ! -d /usr/share/tomcat6/webapps/api.demo.openconext.org ];
 then
+
+	service tomcat6 stop
+	
     cd /tmp    
     git clone git://github.com/OpenConext/OpenConext-api.git
     cd -
@@ -10,18 +13,18 @@ then
     mvn -q clean install -DskipTests    
     cd -
     
-    rm /usr/share/tomcat6/wars/coin-teams-war-*.war 2> /dev/null    
+    rm /usr/share/tomcat6/wars/coin-api-*.war 2> /dev/null    
     cp /tmp/OpenConext-api/coin-api-war/target/*.war /usr/share/tomcat6/wars
     API_WAR=`basename /usr/share/tomcat6/wars/coin-api-*.war`
     
     rm -rf /tmp/OpenConext-api
     
     mkdir -p /usr/share/tomcat6/conf/Catalina/api.demo.openconext.org
-    echo "<Context path=\"/\" docBase=\"/usr/share/tomcat6/wars/$API_WAR\"/>" > \
-        /usr/share/tomcat6/conf/Catalina/api.demo.openconext.org/ROOT.xml
+    echo "<Context path=\"/v1\" docBase=\"/usr/share/tomcat6/wars/$API_WAR\"/>" > \
+        /usr/share/tomcat6/conf/Catalina/api.demo.openconext.org/v1.xml
 
     mkdir -p /usr/share/tomcat6/webapps/api.demo.openconext.org
     chown -Rf tomcat:tomcat /usr/share/tomcat6/webapps/
 
-    service tomcat6 restart
+    service tomcat6 start
 fi
