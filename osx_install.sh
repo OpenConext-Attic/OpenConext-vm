6ef674d8d45e43d2d08e2092f11ce38e5d9b817b
oharsta <oharsta@zilverline.com>
2012-06-27 09:19:02 +0200

    Removed && from osx_install.sh

--- osx_install.sh
+++ osx_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-vagrant basebox build -f demo.openconext.org &&
-vagrant basebox export demo.openconext.org &&
+vagrant basebox build -f demo.openconext.org 
+vagrant basebox export demo.openconext.org 
 
 vagrant box remove demo.openconext.org
 vagrant box add demo.openconext.org demo.openconext.org.box
