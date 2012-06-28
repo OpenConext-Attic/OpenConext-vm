653b976eab6e2f323a9e60daa9c07c34c8275ba7
Alle Veenstra <alle.veenstra@gmail.com>
2012-06-28 13:23:02 +0200

    Versions

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,11 +2,11 @@
 
 # these versions are tag names
 export ENGINEBLOCK_VERSION=engineblock-v2.6.0
-export API_VERSION=coin-api-parent-2.8.4
+export API_VERSION=coin-api-parent-2.8.5
 export MANAGE_VERSION=manage-v2.5.0
 export SERVICEREGISTRY_VERSION=serviceregistry-v2.6.0
 export STATIC_VERSION=static-v2.6.0
-export TEAMS_VERSION=coin-teams-parent-2.8.0
+export TEAMS_VERSION=coin-teams-parent-2.8.1
 export MUJINA_VERSION=mujina-2.8.0
 
 # these are _not_ tag names
