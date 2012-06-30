107b5415a0148161b93e79263f49c5c2a2fa21c3
oharsta <oharsta@zilverline.com>
2012-06-30 20:39:15 +0200

    Added versions_head.sh for development modus

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # these versions are tag names
-export ENGINEBLOCK_VERSION=engineblock-v2.6.0
-export API_VERSION=coin-api-parent-2.8.5
-export MANAGE_VERSION=manage-v2.5.0
-export SERVICEREGISTRY_VERSION=serviceregistry-v2.6.0
-export STATIC_VERSION=static-v2.6.0
-export TEAMS_VERSION=coin-teams-parent-2.8.1
-export MUJINA_VERSION=mujina-2.8.0
+export ENGINEBLOCK_VERSION=HEAD
+export API_VERSION=HEAD
+export MANAGE_VERSION=HEAD
+export SERVICEREGISTRY_VERSION=HEAD
+export STATIC_VERSION=HEAD
+export TEAMS_VERSION=HEAD
+export MUJINA_VERSION=HEAD
 
 # these are _not_ tag names
 export GROUPER_VERSION=2.8.0
