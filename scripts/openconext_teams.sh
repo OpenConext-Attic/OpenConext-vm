601ea576b7d193934b8f506968e7745ac4506fdc
Geert van der Ploeg <geertpl@gmail.com>
2012-07-09 16:26:39 +0200

    BACKLOG-562: removed api and teams properties files (moved to respective dists), changed teams script

--- scripts/openconext_teams.sh
+++ scripts/openconext_teams.sh
@@ -6,25 +6,35 @@ if [ ! -d /usr/share/tomcat6/webapps/teams.demo.openconext.org ];
 then
     cd /tmp
     git clone git://github.com/OpenConext/OpenConext-teams.git
-    cd -
     cd /tmp/OpenConext-teams
     git checkout ${TEAMS_VERSION}
     mvn -q clean install -DskipTests    
-    cd -
+
+		# extract deployable artifact
+		tar -zxf coin-api-dist/target/*-bin.tar.gz -C coin-api-dist/target
+		
+		# remove old deployed war
+    rm /usr/share/tomcat6/wars/coin-teams-war-*.war 2> /dev/null        
+		# copy new war to Tomcat
+    cp /tmp/OpenConext-teams/coin-teams-dist/target/coin-teams-dist*/tomcat/webapps/*.war /usr/share/tomcat6/wars
     
-    rm /usr/share/tomcat6/wars/coin-teams-war-*.war 2> /dev/null    
-    cp /tmp/OpenConext-teams/coin-teams-war/target/*.war /usr/share/tomcat6/wars
-    TEAMS_WAR=`basename /usr/share/tomcat6/wars/coin-teams-*.war`
-    
-    rm -rf /tmp/OpenConext-teams
-    
+		cp /tmp/OpenConext-api/coin-teams-dist/target/coin-teams-dist*/tomcat/conf/classpath_properties/*.vm /usr/share/tomcat6/conf/classpath_properties/
+		# strip off the .vm extension
+		for i in /usr/share/tomcat6/conf/classpath_properties/*.vm
+		do
+			mv $i `dirname $i`/`basename $i .vm`
+		done
+
+   	# create Tomcat-specific context configuration file
+		TEAMS_WAR=`basename /usr/share/tomcat6/wars/coin-teams-*.war`    
     mkdir -p /usr/share/tomcat6/conf/Catalina/teams.demo.openconext.org
     echo "<Context path=\"/teams\" docBase=\"/usr/share/tomcat6/wars/$TEAMS_WAR\"/>" > \
         /usr/share/tomcat6/conf/Catalina/teams.demo.openconext.org/teams.xml
     
     echo "create database teams default charset utf8 default collate utf8_unicode_ci;" | mysql -u root --password=c0n3xt
-	mysql -u root --password=c0n3xt teams < /vagrant/data/teams.sql
+		mysql -u root --password=c0n3xt teams < /vagrant/data/teams.sql
       
     mkdir -p /usr/share/tomcat6/webapps/teams.demo.openconext.org
     chown -Rf tomcat:tomcat /usr/share/tomcat6/webapps/
+		rm -rf /tmp/OpenConext-teams
 fi
