e08277611ce743b8826812e627fc5c01ce184a2b
oharsta <oharsta@zilverline.com>
2012-09-20 06:52:11 +0200

    Maven binary has moved, fixed the maven install script

--- scripts/maven_install.sh
+++ scripts/maven_install.sh
@@ -6,7 +6,7 @@ mvn -v > /dev/null
 if [ $? -ne 0 ]
 then
     cd /opt &&
-    wget -q http://apache.cs.uu.nl/dist//maven/binaries/apache-maven-${MVN_VERSION}-bin.tar.gz &&
+    wget -q http://apache.cs.uu.nl/dist/maven/maven-3/${MVN_VERSION}/binaries/apache-maven-${MVN_VERSION}-bin.tar.gz &&
     tar -xzf apache-maven-${MVN_VERSION}-bin.tar.gz &&
     rm apache-maven-${MVN_VERSION}-bin.tar.gz &&
     ln -sf apache-maven-${MVN_VERSION} maven &&
