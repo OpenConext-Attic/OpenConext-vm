becb80dfbff0db2592932bc176c8073979636aec
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2012-11-28 10:07:13 +0100

    moved logging to engineblock script, before migrate part

--- scripts/components/engineblock.sh
+++ scripts/components/engineblock.sh
@@ -48,6 +48,12 @@ then
     cd -
 fi
 
+mkdir -p /var/log/surfconext
+# TODO: is this chmod really neccessary?
+chmod o+w /var/log/surfconext
+touch /var/log/surfconext/engineblock.log
+chmod o+w /var/log/surfconext/engineblock.log
+
 cd /opt/www/engineblock/
 ./bin/migrate
 cd -
@@ -71,8 +77,3 @@ cat $OC_BASEDIR/configs/httpd/conf.d/profile.conf  | \
 # Make public key available for other components
 ENGINEBLOCK_CERT=`sed '1d;$d' /etc/surfconext/engineblock.crt | tr -d '\n'`
 
-mkdir -p /var/log/surfconext
-# TODO: is this chmod really neccessary?
-chmod o+w /var/log/surfconext
-touch /var/log/surfconext/engineblock.log
-chmod o+w /var/log/surfconext/engineblock.log
