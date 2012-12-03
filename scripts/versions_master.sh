305c6d22d1f30f419e7828782857ad58b1882f6d
oharsta <oharsta@zilverline.com>
2012-12-03 11:01:41 +0100

    Upped versions to master and added iptables information in READM

---
+++ scripts/versions_master.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# these versions are tag names
+export ENGINEBLOCK_VERSION=master
+export API_VERSION=master
+export MANAGE_VERSION=master
+export SERVICEREGISTRY_VERSION=master
+export STATIC_VERSION=master
+export TEAMS_VERSION=master
+export MUJINA_VERSION=master
+
+# these are _not_ tag names
+export GROUPER_VERSION=2.8.0
