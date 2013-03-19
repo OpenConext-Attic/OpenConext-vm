c0ff8e3dc90688803ab816c4b41ec4b261691a65
Remold Krol <remold@everett.nl>
2013-03-19 23:17:18 +0100

    Add versions_v46.sh

---
+++ scripts/versions_v46.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# these versions are tag names
+export ENGINEBLOCK_VERSION=3.1.0
+export API_VERSION=coin-api-parent-3.1.1
+export MANAGE_VERSION=3.0.0
+export SERVICEREGISTRY_VERSION=3.1.0
+export STATIC_VERSION=static-v2.12.2
+export TEAMS_VERSION=coin-teams-parent-3.1.2
+export MUJINA_VERSION=mujina-2.15.1
+
+# these are _not_ tag names
+export GROUPER_VERSION=2.8.0
