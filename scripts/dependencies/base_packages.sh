2e11e37f28ddf17782915c50773c79effb5db0c0
Geert van der Ploeg <geert.vanderploeg@flex.surfnet.nl>
2013-09-11 16:01:54 +0200

    Added sudo to base packages

--- scripts/dependencies/base_packages.sh
+++ scripts/dependencies/base_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-$YUM -y install git subversion policycoreutils-python ntp
+$YUM -y install git subversion policycoreutils-python ntp sudo
 
 # Sync clock with ntp server (vm's tend to have their clock lag behind)
 chkconfig --level 235 ntpd on
