32489b2828e601d32219033ea90b044823f513b2
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2014-02-21 12:15:01 +0100

    improved the check on RHEL repository

--- scripts/dependencies/base_packages.sh
+++ scripts/dependencies/base_packages.sh
@@ -2,12 +2,13 @@
 
 #for some packages (like php extension mcrypt) we need the extra repo
 #from Redhat Enterprise Linux
-if [[ `rpm -qi epel-release-6-8` ]]
+if [ `rpm -qi epel-release-6-8 | grep 'not installed'` ]
 then
-  echo "REL repository already installed"
-else
+  echo "install additional RHEL repository"
   curl -o /tmp/epel-release-6-8.noarch.rpm http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
   rpm -Uvh /tmp/epel-release-6-8.noarch.rpm
+else
+  echo "RHEL repository already installed"
 fi
 
 $YUM -y install git subversion policycoreutils-python ntp sudo
