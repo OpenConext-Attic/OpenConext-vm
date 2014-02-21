a8d59cf21cb5eac8480674d99901b1ce0fb94a01
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2014-02-21 11:09:20 +0100

    added RHEL repo for the openconext vm

--- scripts/dependencies/base_packages.sh
+++ scripts/dependencies/base_packages.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#for some packages (like php extension mcrypt) we need the extra repo
+#from Redhat Enterprise Linux
+curl -o /tmp/epel-release-6-8.noarch.rpm http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
+rpm -Uvh /tmp/epel-release-6-8.noarch.rpm
 
 $YUM -y install git subversion policycoreutils-python ntp sudo
 
