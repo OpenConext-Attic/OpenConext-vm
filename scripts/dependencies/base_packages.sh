43eddfc01030bc6c4f5f1f81daf6bd67f5581127
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2014-02-21 12:21:04 +0100

    fixed if statement

--- scripts/dependencies/base_packages.sh
+++ scripts/dependencies/base_packages.sh
@@ -2,7 +2,7 @@
 
 #for some packages (like php extension mcrypt) we need the extra repo
 #from Redhat Enterprise Linux
-if [ `rpm -qi epel-release-6-8 | grep 'not installed'` ]
+if [[ `rpm -qi epel-release-6-8 | grep 'not installed'` ]]
 then
   echo "install additional RHEL repository"
   curl -o /tmp/epel-release-6-8.noarch.rpm http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
