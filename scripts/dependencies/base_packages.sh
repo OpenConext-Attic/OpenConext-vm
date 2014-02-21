3721cd118026f8c9d8693655a5e6c65e211094fe
Jeroen M Snoeij <jeroen.snoeij@flex.surfnet.nl>
2014-02-21 12:02:06 +0100

    added RHEL repository to the base packages in the VM

--- scripts/dependencies/base_packages.sh
+++ scripts/dependencies/base_packages.sh
@@ -2,8 +2,13 @@
 
 #for some packages (like php extension mcrypt) we need the extra repo
 #from Redhat Enterprise Linux
-curl -o /tmp/epel-release-6-8.noarch.rpm http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
-rpm -Uvh /tmp/epel-release-6-8.noarch.rpm
+if [[ `rpm -qi epel-release-6-8` ]]
+then
+  echo "REL repository already installed"
+else
+  curl -o /tmp/epel-release-6-8.noarch.rpm http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
+  rpm -Uvh /tmp/epel-release-6-8.noarch.rpm
+fi
 
 $YUM -y install git subversion policycoreutils-python ntp sudo
 
