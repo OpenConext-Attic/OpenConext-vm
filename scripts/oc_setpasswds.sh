271b5e2b87712dd79abcacfe96a6e45dbb08249c
Niels van Dijk <niels.vandijk@surfnet.nl>
2014-03-07 17:53:21 +0100

    Added JANUS SALT and CA_KEY_PASS to parameters

--- scripts/oc_setpasswds.sh
+++ scripts/oc_setpasswds.sh
@@ -8,18 +8,33 @@ config_file=oc_config
 # create a backup of the config file
 `cp $config_file $config_file.bak`
 
-# Find config params to change
+# Find generic passowrds (start with OC__, end with _PASS)
 pw_entries=`cat $config_file | grep "_PASS" | grep "OC__"`
 
 for pw_entry in $pw_entries
 do
   key=$(echo $pw_entry | cut -f1 -d=)
-  newvalue=`tr -cd '[:alnum:]' < /dev/urandom | fold -w15 | head -n1`
+  newvalue=`tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1`
   newkeyvaluepair="$key=\"$newvalue\""
   
   echo -e "Setting "$key" = \""$newvalue"\""
 
-  `sed -i "/$key/c\$newkeyvaluepair       # This line was updated by oc_setpasswds.sh." $config_file`
+  `sed -i "/$key/c\$newkeyvaluepair" $config_file`
 done
+
+
+# Now update some specific passwords and keys
+# CA private key
+ca_key=`tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1`
+`sed -i "/CA_KEY_PASSWORD/c\CA_KEY_PASSWORD=\"$ca_key\"" $config_file`
+echo -e "Setting CA_KEY_PASSWORD = \""$ca_key"\""
+
+# ServiceRegistry (Janus) SALT
+salt=`tr -cd '[:alnum:]' < /dev/urandom | fold -w40 | head -n1`
+`sed -i "/OC__JANUS_SECRETSALT/c\OC__JANUS_SECRETSALT=\"$salt\"" $config_file`
+echo -e "Setting OC__JANUS_SECRETSALT = \""$salt"\""
+
+
+
 
 
