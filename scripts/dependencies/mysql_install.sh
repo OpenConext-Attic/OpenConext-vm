50952a7c78dde8a87fa87e3fb43e0cd9428d1f03
surfnet-niels <niels.vandijk@surfnet.nl>
2014-03-20 16:27:33 +0100

    Added check for new db root password

--- scripts/dependencies/mysql_install.sh
+++ scripts/dependencies/mysql_install.sh
@@ -4,12 +4,14 @@ $YUM -y install mysql mysql-server mysql-connector-java
 service mysqld restart
 chkconfig mysqld on
 
-# configure MySQL (WARNING: this is insecure!)
-
-#if ! mysqladmin -u root --password=$OC__ROOT_DB_PASS ping 2> /dev/null | grep -q "mysqld is alive"
-#then
-#	mysqladmin -u root password $OC__ROOT_DB_PASS
-#fi
+# configure MySQL
 echo -e "\n\nSetting new MySQL Root password \n\n"
 mysqladmin -u root password $OC__ROOT_DB_PASS
-echo "grant all privileges on *.* to 'root'@'%' identified by '$OC__ROOT_DB_PASS';" | mysql -u root --password=$OC__ROOT_DB_PASS mysql
+#echo "grant all privileges on *.* to 'root'@'localhost' identified by '$OC__ROOT_DB_PASS';" | mysql -u root --password=$OC__ROOT_DB_PASS mysql
+
+if ! mysqladmin -u root --password=$OC__ROOT_DB_PASS ping 2> /dev/null | grep -q "mysqld is alive"
+	echo -e "\n\nSetting new MySQL Root password FAILED\n\n"
+	exit
+then
+        echo -e "\n\nSetting new MySQL Root password OK\n\n"     
+fi
