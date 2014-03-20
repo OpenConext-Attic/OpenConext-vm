e49636eddb006242c2ff9563fedb54ff059fd721
surfnet-niels <niels.vandijk@surfnet.nl>
2014-03-20 16:41:15 +0100

    yup

--- scripts/dependencies/mysql_install.sh
+++ scripts/dependencies/mysql_install.sh
@@ -10,8 +10,9 @@ mysqladmin -u root password $OC__ROOT_DB_PASS
 #echo "grant all privileges on *.* to 'root'@'localhost' identified by '$OC__ROOT_DB_PASS';" | mysql -u root --password=$OC__ROOT_DB_PASS mysql
 
 if ! mysqladmin -u root --password=$OC__ROOT_DB_PASS ping 2> /dev/null | grep -q "mysqld is alive"
-        echo -e "\n\nSetting new MySQL Root password OK\n\n"     
 then
+        echo -e "\n\nSetting new MySQL Root password OK\n\n"     
+else
 	echo -e "\n\nSetting new MySQL Root password FAILED\n\n"
 	exit
 fi
