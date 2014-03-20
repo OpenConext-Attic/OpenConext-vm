3f59fa54e263a3b21d360ab0136669a09b3b769c
surfnet-niels <niels.vandijk@surfnet.nl>
2014-03-20 16:02:43 +0100

    Fixed error in setting root passwd or MySQL

--- scripts/dependencies/mysql_install.sh
+++ scripts/dependencies/mysql_install.sh
@@ -6,8 +6,8 @@ chkconfig mysqld on
 
 # configure MySQL (WARNING: this is insecure!)
 
-if ! mysqladmin -u root --password=$ROOT_DB_PASS ping 2> /dev/null | grep -q "mysqld is alive"
+if ! mysqladmin -u root --password=$OC__ROOT_DB_PASS ping 2> /dev/null | grep -q "mysqld is alive"
 then
-	mysqladmin -u root password $ROOT_DB_PASS
+	mysqladmin -u root password $OC__ROOT_DB_PASS
 fi
-echo "grant all privileges on *.* to 'root'@'%' identified by '$ROOT_DB_PASS';" | mysql -u root --password=$ROOT_DB_PASS mysql
+echo "grant all privileges on *.* to 'root'@'%' identified by '$OC__ROOT_DB_PASS';" | mysql -u root --password=$OC__ROOT_DB_PASS mysql
