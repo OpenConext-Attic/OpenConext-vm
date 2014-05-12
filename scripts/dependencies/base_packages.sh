706cf81c68e63b08d37fef2b2526fc3a9e5a2a86
Boy Baukema <boy.baukema@flex.surfnet.nl>
2014-05-12 16:12:53 +0200

    Use YUM to install packages to it's index is always up to date

--- scripts/dependencies/base_packages.sh
+++ scripts/dependencies/base_packages.sh
@@ -6,7 +6,8 @@ if [[ `rpm -qi epel-release-6-8 | grep 'not installed'` ]]
 then
   echo "install additional RHEL repository"
   curl -o /tmp/epel-release-6-8.noarch.rpm http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
-  rpm -Uvh /tmp/epel-release-6-8.noarch.rpm
+  yum install -y /tmp/epel-release-6-8.noarch.rpm
+  rm /tmp/epel-release-6-8.noarch.rpm
 else
   echo "RHEL repository already installed"
 fi
