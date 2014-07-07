491f5a1761b0bf8b19b04c0ac4482c738ffe64df
Boy Baukema <boy@ibuildings.nl>
2014-07-07 17:02:24 +0200

    Moved roles to own repositories. Introduced Librarian Ansible. Restructured Ansible setup.

--- scripts/components/engineblock.sh
+++ scripts/components/engineblock.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Make sure Ansible is installed
-yum install -y ansible MySQL-python &&
+# Make sure Ansible is installed cleanly
+sudo yum erase -y ansible &&
+sudo yum install -y ansible MySQL-python patch &&
+
+# Patch Ansible for: #8050: ini_file module still changes all option names to lower case.
+# -b = backup, -N = assume merged if failed, -u = Unified format, -p0 = 0 leading lines, -d chdir to dir before apply.
+sudo patch -b -N -u -p0 -d /usr/share/ansible/files < $OC_BASEDIR/patches/ansible/ansible/ini_file_keep_key_casing-issue_8050-ansible_1.6.2.diff
 
 # Run Ansible playbook
-ansible-playbook \
+sudo ansible-playbook \
   -v \
   -i $OC_BASEDIR/tools/ansible/inventory/demo.openconext.org.ini \
   -e "openconext_domain=$OC_DOMAIN" \
@@ -21,6 +26,6 @@ ansible-playbook \
   -e "serviceregistry_url=https://serviceregistry.$OC_DOMAIN/simplesaml/module.php/janus/services/rest/" \
   -e "serviceregistry_user=$OC__ENGINE_JANUSAPI_USER" \
   -e "serviceregistry_secret=$OC__ENGINE_JANUSAPI_PASS" \
-  $OC_BASEDIR/tools/ansible/provision/engine.yml
+  $OC_BASEDIR/tools/ansible/engineblock.yml
 
 ENGINEBLOCK_CERT=`sed '1d;$d' /etc/surfconext/engineblock.crt | tr -d '\n'`
