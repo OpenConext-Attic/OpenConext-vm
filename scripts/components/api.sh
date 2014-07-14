4905b8150bce23c03aa22f34521745ba24fdbd51
Boy Baukema <boy@ibuildings.nl>
2014-07-14 17:29:15 +0200

    Bugfixes after testing Ansible roles

--- scripts/components/api.sh
+++ scripts/components/api.sh
@@ -9,33 +9,34 @@ sudo yum install -y ansible MySQL-python patch python-lxml &&
 sudo patch -b -N -u -p0 -d /usr/share/ansible/files < $OC_BASEDIR/patches/ansible/ansible/ini_file_keep_key_casing-issue_8050-ansible_1.6.2.diff
 
 # Run Ansible playbook
+echo "Provisioning API with Ansible..."
 sudo ansible-playbook \
   -v \
   -i /vagrant/tools/ansible/inventory/demo.openconext.org.ini \
-  -e "openconext_domain=demo.openconext.org" \
-  -e "api_version=feature/ansible" \
+  -e "api_version=$API_VERSION" \
   -e "api_db_name=api" \
   -e "api_db_host=localhost" \
   -e "api_db_port=3306" \
-  -e "api_db_user=api" \
-  -e "api_db_password=api" \
+  -e "api_db_user=$OC__API_DB_USER" \
+  -e "api_db_password=$OC__API_DB_PASS" \
+  -e "api_janus_url=https://serviceregistry.$OC_DOMAIN/simplesaml/module.php/janus/services/rest/" \
+  -e "api_janus_user=$OC__API_JANUSAPI_USER" \
+  -e "api_janus_secret=$OC__API_JANUSAPI_PASS" \
+  -e "api_sp_key_string=$OC_KEY" \
+  -e "api_sp_cert_string=$OC_CERT" \
+  -e "api_ldap_bind_dn=$OC__LDAP_USER" \
+  -e "api_ldap_password=$OC__LDAP_PASS" \
+  -e "db_admin_user=root" \
+  -e "db_admin_password=$OC__ROOT_DB_PASS" \
   -e "engine_db_name=engineblock" \
   -e "engine_db_host=localhost" \
   -e "engine_db_port=3306" \
-  -e "engine_db_user=root" \
-  -e "engine_db_password=c0n3xt" \
+  -e "engine_db_user=$OC__ENGINE_DB_USER" \
+  -e "engine_db_password=$OC__ENGINE_DB_PASS" \
+  -e "engine_idp_cert_string=$ENGINEBLOCK_CERT" \
+  -e "openconext_domain=$OC_DOMAIN" \
+  -e "openconext_error_mail=" \
   -e "teams_db_name=" \
   -e "teams_db_user=" \
   -e "teams_db_password=" \
-  -e "openconext_error_mail=" \
-  -e "engine_idp_cert_string=abcdef1234567890" \
-  -e "api_sp_key_string=abcdef1234567890" \
-  -e "api_sp_cert_string=abcdef1234567890" \
-  -e "api_ldap_bind_dn=ldap" \
-  -e "api_ldap_password=ldap" \
-  -e "db_admin_user=root" \
-  -e "db_admin_password=c0n3xt" \
-  -e "api_janus_url=https://serviceregistry.demo.openconext.org/simplesaml/module.php/janus/services/rest/" \
-  -e "api_janus_user=engine" \
-  -e "api_janus_password=engine" \
-  /vagrant/tools/ansible/api.yml
+  $OC_BASEDIR/tools/ansible/api.yml
--- scripts/components/engineblock.sh
+++ scripts/components/engineblock.sh
@@ -6,26 +6,29 @@ sudo yum install -y ansible MySQL-python patch &&
 
 # Patch Ansible for: #8050: ini_file module still changes all option names to lower case.
 # -b = backup, -N = assume merged if failed, -u = Unified format, -p0 = 0 leading lines, -d chdir to dir before apply.
-sudo patch -b -N -u -p0 -d /usr/share/ansible/files < $OC_BASEDIR/patches/ansible/ansible/ini_file_keep_key_casing-issue_8050-ansible_1.6.2.diff
+sudo patch -b -N -u -p0 -d /usr/share/ansible/files < $OC_BASEDIR/patches/ansible/ansible/ini_file_keep_key_casing-issue_8050-ansible_1.6.2.diff &&
 
 # Run Ansible playbook
+echo "Provisioning EngineBlock with Ansible..." &&
 sudo ansible-playbook \
   -v \
   -i $OC_BASEDIR/tools/ansible/inventory/demo.openconext.org.ini \
-  -e "openconext_domain=$OC_DOMAIN" \
-  -e "engine_version=$ENGINEBLOCK_VERSION" \
+  -e "db_admin_user=root" \
+  -e "db_admin_password=$OC__ROOT_DB_PASS" \
   -e "engine_db_name=engineblock" \
   -e "engine_db_host=localhost" \
   -e "engine_db_port=3306" \
   -e "engine_db_user=$OC__ENGINE_DB_USER" \
   -e "engine_db_password=$OC__ENGINE_DB_PASS" \
-  -e "engine_ldap_binddn=cn:engine,dc:surfconext,dc:nl" \
-  -e "engine_ldap_password=$OC__LDAP_PASS" \
-  -e "db_admin_user=root" \
-  -e "db_admin_password=$OC__ROOT_DB_PASS" \
   -e "engine_janus_url=https://serviceregistry.$OC_DOMAIN/simplesaml/module.php/janus/services/rest/" \
   -e "engine_janus_user=$OC__ENGINE_JANUSAPI_USER" \
   -e "engine_janus_secret=$OC__ENGINE_JANUSAPI_PASS" \
+  -e "engine_ldap_binddn=cn=engine,dc=surfconext,dc=nl" \
+  -e "engine_ldap_password=$OC__LDAP_PASS" \
+  -e "engine_version=$ENGINEBLOCK_VERSION" \
+  -e "ldap_admin_bind_dn=$OC__LDAPADMIN_USER" \
+  -e "ldap_admin_password=$OC__LDAPADMIN_PASS" \
+  -e "openconext_domain=$OC_DOMAIN" \
   $OC_BASEDIR/tools/ansible/engineblock.yml
 
 ENGINEBLOCK_CERT=`sed '1d;$d' /etc/surfconext/engineblock.crt | tr -d '\n'`
