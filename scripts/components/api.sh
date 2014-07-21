f2e233297aa031229d1b1c84c2abf7207d7b4da4
Boy Baukema <boy@ibuildings.nl>
2014-07-21 09:53:48 +0200

    Removed defaults from API provisioning and added credentials for teams database

--- scripts/components/api.sh
+++ scripts/components/api.sh
@@ -14,29 +14,20 @@ sudo ansible-playbook \
   -v \
   -i /vagrant/tools/ansible/inventory/demo.openconext.org.ini \
   -e "api_version=$API_VERSION" \
-  -e "api_db_name=api" \
-  -e "api_db_host=localhost" \
-  -e "api_db_port=3306" \
   -e "api_db_user=$OC__API_DB_USER" \
   -e "api_db_password=$OC__API_DB_PASS" \
-  -e "api_janus_url=https://serviceregistry.$OC_DOMAIN/simplesaml/module.php/janus/services/rest/" \
   -e "api_janus_user=$OC__API_JANUSAPI_USER" \
   -e "api_janus_secret=$OC__API_JANUSAPI_PASS" \
   -e "api_sp_key_string=$OC_KEY" \
   -e "api_sp_cert_string=$OC_CERT" \
   -e "api_ldap_bind_dn=$OC__LDAP_USER" \
   -e "api_ldap_password=$OC__LDAP_PASS" \
-  -e "db_admin_user=root" \
   -e "db_admin_password=$OC__ROOT_DB_PASS" \
-  -e "engine_db_name=engineblock" \
-  -e "engine_db_host=localhost" \
-  -e "engine_db_port=3306" \
   -e "engine_db_user=$OC__ENGINE_DB_USER" \
   -e "engine_db_password=$OC__ENGINE_DB_PASS" \
   -e "engine_idp_cert_string=$ENGINEBLOCK_CERT" \
   -e "openconext_domain=$OC_DOMAIN" \
-  -e "openconext_error_mail=" \
-  -e "teams_db_name=" \
-  -e "teams_db_user=" \
-  -e "teams_db_password=" \
+  -e "openconext_error_mail=$OC__ADMIN_EMAIL" \
+  -e "teams_db_user=$OC__TEAMS_DB_USER" \
+  -e "teams_db_password=$OC__TEAMS_DB_PASS" \
   $OC_BASEDIR/tools/ansible/api.yml
