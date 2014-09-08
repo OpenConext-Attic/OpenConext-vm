d8540b46c7227ee7a422ed15338e3b4267e3c299
Remold Krol <remold.krol@everett.nl>
2014-09-08 14:39:36 +0200

    versions.sh for R62

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 # these versions are tag names
-export ENGINEBLOCK_VERSION=develop
-export API_VERSION=develop
-export MANAGE_VERSION=develop
-export SERVICEREGISTRY_VERSION=develop
-export STATIC_VERSION=develop
-export TEAMS_VERSION=develop
-export MUJINA_VERSION=develop
-export CRUNCHER_VERSION=develop
-export APIS_VERSION=develop
-export CSA_VERSION=develop
-export SELFSERVICE_VERSION=develop
+export ENGINEBLOCK_VERSION=3.8.1
+export API_VERSION=coin-api-parent-3.8.0
+export MANAGE_VERSION=3.1.0
+export SERVICEREGISTRY_VERSION=3.9.1
+export STATIC_VERSION=static-v2.12.2
+export TEAMS_VERSION=coin-teams-parent-3.3.0
+export MUJINA_VERSION=mujina-3.9.0
+export CRUNCHER_VERSION=cruncher-parent-3.12.0
+export APIS_VERSION=apis-parent-1.3.4
+export CSA_VERSION=csa-parent-3.8.0
+export SELFSERVICE_VERSION=coin-selfservice-parent-3.12.0
 
 # these are _not_ tag names
-export GROUPER_DIST_VERSION=4.0.0
+export GROUPER_DIST_VERSION=3.13.0
 export GROUPER_VERSION=2.1.5
 
 # the version of this install
-export OC_VERSION=develop
+export OC_VERSION=v62
