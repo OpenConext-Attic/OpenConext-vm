c1e6f694c27273e316b274e05ef4567571593724
Remold Krol <remold.krol@everett.nl>
2014-09-15 15:43:39 +0200

    Set versions.sh to r74.4

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # these versions are tag names
-export ENGINEBLOCK_VERSION=3.8.1
-export API_VERSION=coin-api-parent-3.8.0
-export MANAGE_VERSION=3.1.0
-export SERVICEREGISTRY_VERSION=3.9.1
+export ENGINEBLOCK_VERSION=4.1.5
+export API_VERSION=coin-api-parent-4.0.0
+export MANAGE_VERSION=4.0.0
+export SERVICEREGISTRY_VERSION=4.0.6
 export STATIC_VERSION=static-v2.12.2
-export TEAMS_VERSION=coin-teams-parent-3.3.0
-export MUJINA_VERSION=mujina-3.9.0
+export TEAMS_VERSION=coin-teams-parent-4.0.0
+export MUJINA_VERSION=mujina-4.0.1
 export CRUNCHER_VERSION=cruncher-parent-3.12.0
-export APIS_VERSION=apis-parent-1.3.4
-export CSA_VERSION=csa-parent-3.8.0
-export SELFSERVICE_VERSION=coin-selfservice-parent-3.12.0
+export APIS_VERSION=apis-parent-1.3.5
+export CSA_VERSION=csa-parent-4.0.3.1
+export SELFSERVICE_VERSION=coin-selfservice-parent-4.0.2
 
 # these are _not_ tag names
-export GROUPER_DIST_VERSION=3.13.0
+export GROUPER_DIST_VERSION=4.0.0
 export GROUPER_VERSION=2.1.5
 
 # the version of this install
-export OC_VERSION=v62
+export OC_VERSION=v74.4
